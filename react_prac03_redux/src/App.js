af33923a08f4d5a739973384b95a771e6c63dae0
HaeRheeNam <hanm3208@gmail.com>
2022-10-08 17:46:59 +0900

    +payload_quiz

--- react_prac03_redux/src/App.js
+++ react_prac03_redux/src/App.js
@@ -1,11 +1,41 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addNumber } from "./redux/modules/counter";
+import { minusNumber } from "./redux/modules/counter";
 
 function App() {
-  //컴포넌트에서 스토어 조회가능
-  const counterStore = useSelector((state) => state);
+  //1.dispatch를 사용하기 위해 선언
+  const dispatch = useDispatch();
+  const [number, setNumber] = useState(0);
+  const globalNumber = useSelector((state) => state.counter.number);
 
-  return <div className="App">생성</div>;
+  //input의 값 잡고있기
+  const onChangeHandler = (e) => {
+    const { value } = e.target;
+    //event.target.value는 문자열이라
+    // 이걸 숫자형으로 형변환해주기 위해서 +를 붙여줌.
+    setNumber(+value);
+  };
+  //2. 더하기 버튼 눌렀을 때 실행할 이벤트핸들러
+  const onClickAddNumberHandler = () => {
+    //5. Action creator를 dispatch해주고, 그때 Action creator의 인자에
+    //number를 넣어줘
+    dispatch(addNumber(number));
+  };
+  const onClickMinusNum = () => {
+    //5. Action creator를 dispatch해주고, 그때 Action creator의 인자에
+    //number를 넣어줘
+    dispatch(minusNumber(number));
+  };
+
+  return (
+    <div>
+      <div>{globalNumber}</div>
+      <input type="number" onChange={onChangeHandler} />
+      <button onClick={onClickAddNumberHandler}>더하기</button>
+      <button onClick={onClickMinusNum}>빼기</button>
+    </div>
+  );
 }
 
 export default App;
---
+++ react_prac03_redux/src/redux/modules/counter.js
@@ -0,0 +1,50 @@
+// src/redux/modules/counter.js
+
+// import { action } from "@remix-run/router";
+
+// Action Value
+// const plusOne = () => {};
+
+//Action Value
+const ADD_NUMBER = "ADD_NUMBER";
+const MINUS_NUMBER = "MINUS_NUMBER";
+
+// Action Creator
+export const addNumber = (payload) => {
+  return {
+    type: ADD_NUMBER,
+    payload: payload,
+  };
+};
+
+export const minusNumber = (payload) => {
+  return {
+    type: MINUS_NUMBER,
+    payload: payload,
+  };
+};
+
+// Initial State
+const initialState = {
+  number: 0,
+};
+
+// Reducer
+const counter = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_NUMBER:
+      return {
+        //state.number (기존의 number)에
+        //action.payload(유저가 더하길 원하는 값)을 더한다.
+        number: state.number + action.payload,
+      };
+    case MINUS_NUMBER:
+      return {
+        number: state.number - action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default counter;
